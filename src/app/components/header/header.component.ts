e2c7536761368f75bc801698e5a6291fdf6db407
joseyzambranov <joseyzambranovpe@gmail.com>
2022-12-09 13:08:58 -0500

    added template and styles

---
+++ src/app/components/header/header.component.ts
@@ -0,0 +1,40 @@
+import { Component, OnInit, Renderer2 } from '@angular/core';
+
+@Component({
+  selector: 'app-header',
+  templateUrl: "./header.component.html",
+  styleUrls: ['./header.component.css']
+})
+export class HeaderComponent implements OnInit {
+  flag = false;
+  dark = false;
+  public selected = false;
+  public sections = 4;
+  public scroll!: number; 
+
+  constructor(
+    private renderer: Renderer2
+  ) { }
+
+  ngOnInit() {
+    this.renderer.listen(window, 'scroll', ($event) => {
+      this.scroll = (window.scrollY / this.sections);
+  })
+  }
+
+  showMenu(){
+    this.flag = !this.flag;
+  }
+  removeMenu(){
+    this.flag = false;      
+  }
+  darkTheme(){
+    this.dark = !this.dark;
+    if(this.dark){
+      this.renderer.addClass(document.body, 'dark-theme'); 
+    }else{
+      this.renderer.removeClass(document.body, 'dark-theme');
+    }    
+  }
+
+}
